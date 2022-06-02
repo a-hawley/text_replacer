7019133be99de72b23a7dac3c9be85adec29966e
Alec Hawley <alech35@yahoo.com>
2022-06-01 22:38:28 -0500

    Added error handler for exiting

--- replacer.py
+++ replacer.py
@@ -1,4 +1,4 @@
-from dataclasses import replace
+
 from easygui import fileopenbox
 from fileinput import FileInput
 
@@ -8,34 +8,40 @@ def text_replacer(search, change):
     extensions = ['docx', 'txt']
 
     # Select file
-    file_path = fileopenbox()
+    try:
+        file_path = fileopenbox()
 
-    # Check extension for validity
-    file_path_checker = file_path.split('.')
+        # Check extension for validity
+        file_path_checker = file_path.split('.')
 
-    if file_path_checker[1] not in extensions:
-
-        print('Sorry, this program can\'t access that file type.')
-
-    elif file_path_checker[1] == 'txt':
-
-        # Open, read, overwrite keywords, save.
-        with FileInput(file_path, inplace=True) as file:
-            for line in file:
-                print(line.replace(search, change))
-
-        print(f'All instances of {search} have been replaced with {change}!')
+    except AttributeError:
+        print('Program exited...')
 
     else:
-        print("Still working on that feature!")
+        if file_path_checker[1] not in extensions:
 
-        # Open and read .docx file
+            print('Sorry, this program can\'t access that file type.')
 
-        # Search for keywords
+        elif file_path_checker[1] == 'txt':
 
-        # Replace keywords with change
+            # Open, read, overwrite keywords, save.
+            with FileInput(file_path, inplace=True) as file:
+                for line in file:
+                    print(line.replace(search, change))
 
-        # Save file
+            print(
+                f'All instances of {search} have been replaced with {change}!')
+
+        else:
+            print("Still working on that feature!")
+
+            # Open and read .docx file
+
+            # Search for keywords
+
+            # Replace keywords with change
+
+            # Save file
 
 
 text_replacer('POSITION', 'software developer')
