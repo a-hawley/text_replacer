6f7c9be2c760c2622a52e214a1c4e4040c58c595
Alec Hawley <alech35@yahoo.com>
2022-06-01 21:16:46 -0500

    Added file box function and validity checker, as well as text file for debugging.

--- replacer.py
+++ replacer.py
@@ -1,6 +1,30 @@
-def text_replacer(doc_path, search, change):
-    pass
-    # Select document
-    # Open document
-    # Search for keywords
-    # Replace keywords with
+from easygui import fileopenbox
+from pip import main
+
+
+def text_replacer(search, change):
+
+    extensions = ['doc', 'txt']
+
+    # Select file
+    file_path = fileopenbox()
+
+    # Check extension for validity
+    file_path_checker = file_path.split('.')
+
+    if extensions[0] not in file_path_checker and extensions[1] not in file_path_checker:
+        print('Sorry, this program can\'t access that file type.')
+    else:
+
+        # Open and read document
+        file_text = open(file_path, mode='r').read()
+        print(file_text)
+
+        # Search for keywords
+
+        # Replace keywords with change
+
+        # Save file
+
+
+text_replacer(1, 1)
